c352f0f10d9f7caee164de3d7dbca8c2936ac7a1
djpillen@umich.edu <djpillen@umich.edu>
2016-06-29 16:30:02 -0400

    javascript not ruby....

--- frontend/assets/generate_resource_identifiers.js
+++ frontend/assets/generate_resource_identifiers.js
@@ -22,7 +22,7 @@ $(function () {
       type: "POST",
       success: function(identifier) {
         identifier_number = pad_number(identifier.number, padding);
-        $('#resource_id_0_').val("#{identifier.year}#{identifier_number}");
+        $('#resource_id_0_').val(identifier.year + identifier_number);
         //$('#resource_id_1_').val(pad_number(identifier.number, padding));
 
         $('#resource_id_1_').enable();
