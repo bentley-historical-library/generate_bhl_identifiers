24edafea747b2895919fa7d0af75a6c750ebc32c
djpillen@umich.edu <djpillen@umich.edu>
2017-06-08 12:21:03 -0400

    finish adding repository id for repo level permissions

--- backend/controllers/generate_4part_id.rb
+++ backend/controllers/generate_4part_id.rb
@@ -4,7 +4,8 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.post('/repositories/:repo_id/plugins/generate_accession_identifiers')
     .description("Initialize the bhl_accession_identifier Sequence")
-    .params(["value", Integer, "The value with which the Sequence will be initialized"])
+    .params(["value", Integer, "The value with which the Sequence will be initialized"],
+            ["repo_id", :repo_id])
     .permissions([:manage_repository])
     .returns([200, "OK"]) \
   do
@@ -13,7 +14,7 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.post('/repositories/:repo_id/plugins/generate_accession_identifiers/next')
     .description("Generate a new incremented accession number")
-    .params()
+    .params(["repo_id", :repo_id])
     .permissions([:update_accession_record])
     .returns([200, "{'number', N}"]) \
   do
@@ -24,7 +25,8 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.post('/repositories/:repo_id/plugins/generate_resource_identifiers')
     .description("Initialize the bhl_resource_identifier Sequence")
-    .params(["value", Integer, "The value with which the Sequence will be initialized"])
+    .params(["value", Integer, "The value with which the Sequence will be initialized"],
+            ["repo_id", :repo_id])
     .permissions([:manage_repository])
     .returns([200, "OK"]) \
   do
@@ -35,7 +37,7 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.post('/repositories/:repo_id/plugins/generate_resource_identifiers/next')
     .description("Generate a new identifier based on the year and a running number")
-    .params()
+    .params(["repo_id", :repo_id])
     .permissions([:update_resource_record])
     .returns([200, "{'year', 'YYYY', 'number', N}"]) \
   do
--- frontend/controllers/generate_resource_identifiers_controller.rb
+++ frontend/controllers/generate_resource_identifiers_controller.rb
@@ -3,7 +3,7 @@ class GenerateResourceIdentifiersController < ApplicationController
   skip_before_filter :unauthorised_access
 
   def generate
-    response = JSONModel::HTTP::post_form('/plugins/generate_resource_identifiers/next')
+    response = JSONModel::HTTP::post_form('/repositories/#{session[:repo_id]}/plugins/generate_resource_identifiers/next')
 
     if response.code == '200'
       render :json => ASUtils.json_parse(response.body)
