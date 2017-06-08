4312faf03b59a10551962fb268574c12662ffae9
djpillen@umich.edu <djpillen@umich.edu>
2017-06-08 12:00:43 -0400

    adding endpoint to initialize sequences

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 This plugin is a slightly modified version of the generate_accession_identifiers plugin that ships with ArchivesSpace.
 It autogenerates accession identifiers (the next sequential number) and resource identifiers (the current year followed by the next sequential three digit number)
 
-The first time this is used, we'll need to initiate the sequences as follows:
+The first time this is used, we'll need to initiate the sequences by posting the current high Accession and Resource values from BEAL to
 
-insert into sequence (sequence_name, value) values ('bhl_accession_identifier', CURRENT_MAX_ACCESSIONID), ('bhl_resource_identifier', CURRENT_MAX_RESOURCEID);
-
+	`/plugins/generate_accession_identifiers?value=current_value`
+	`/plugins/generate_resource_identifiers?value=current_value`
 
 To install, just activate the plugin in your config/config.rb file by
 including an entry such as:
 
-     AppConfig[:plugins] = ['generate-bhl-identifiers']
+     AppConfig[:plugins] = ['generate_bhl_identifiers']
--- backend/controllers/generate_4part_id.rb
+++ backend/controllers/generate_4part_id.rb
@@ -2,10 +2,19 @@ require 'time'
 
 class ArchivesSpaceService < Sinatra::Base
 
+  Endpoint.post('/plugins/generate_accession_identifiers')
+    .description("Initialize the bhl_accession_identifier Sequence")
+    .params(["value", Integer, "The value with which the Sequence will be initialized"])
+    .permissions([:manage_repository])
+    .returns([200, "OK"]) \
+  do
+    Sequence.init("bhl_accession_identifier", params[:value])
+  end
+
   Endpoint.post('/plugins/generate_accession_identifiers/next')
-    .description("Generate a new identifier based on the year and a running number")
+    .description("Generate a new incremented accession number")
     .params()
-    .permissions([])
+    .permissions([:update_accession_record])
     .returns([200, "{'number', N}"]) \
   do
     number = Sequence.get("bhl_accession_identifier")
@@ -13,10 +22,21 @@ class ArchivesSpaceService < Sinatra::Base
     json_response(:number => number)
   end
 
+  Endpoint.post('/plugins/generate_resource_identifiers')
+    .description("Initialize the bhl_resource_identifier Sequence")
+    .params(["value", Integer, "The value with which the Sequence will be initialized"])
+    .permissions([:manage_repository])
+    .returns([200, "OK"]) \
+  do
+    year = Time.now.strftime('%Y')
+    sequence_name = 'bhl_resource_identifer_#{year}'
+    Sequence.init(sequence_name, params[:value])
+  end
+
   Endpoint.post('/plugins/generate_resource_identifiers/next')
     .description("Generate a new identifier based on the year and a running number")
     .params()
-    .permissions([])
+    .permissions([:update_resource_record])
     .returns([200, "{'year', 'YYYY', 'number', N}"]) \
   do
     year = Time.now.strftime('%Y')
